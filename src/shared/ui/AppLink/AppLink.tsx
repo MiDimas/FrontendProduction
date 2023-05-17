7a91fd4e9d11dba76620be2b2b691e12c733b8b7
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-05-17 11:14:30 +0800

    Добавлен ui компонент ссылки с собственными темами

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,29 @@
+import {Link, LinkProps} from "react-router-dom";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./AppLink.module.scss.css";
+import {FC} from "react";
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    INVERTED = 'inverted',
+}
+
+interface AppLinkProps extends LinkProps{
+    className?: string;
+    theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+    const {
+        className,
+        children,
+        to,
+        theme=AppLinkTheme.PRIMARY,
+        ...otherProps
+    } = props;
+    return (
+        <Link to={to} className={classNames(cls.AppLink, {}, [className, cls[theme]])} {...otherProps}>
+            {children}
+        </Link>
+    );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,6 @@
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 
 interface NavbarProps {
     className?: string;
@@ -10,8 +10,8 @@ export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <Link to={'/'} className={cls.mainLink}>Главная</Link>
-                <Link to={'/about'}>О нас</Link>
+                <AppLink theme={AppLinkTheme.INVERTED} to={'/'} className={cls.mainLink}>Главная</AppLink>
+                <AppLink theme={AppLinkTheme.INVERTED} to={'/about'}>О нас</AppLink>
             </div>
 
         </div>
