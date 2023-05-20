20b93eb1f26ca505253f7b26b948a2b0e3f403de
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-05-20 19:02:45 +0800

    Установлены библиотеки i18next для работы с языками
    Добавлен конфигурационный файл, и подготовлен импорт
    переменной сообщающей внутри проекта продакшн или
    дев сборка из вебпака, при помощи плагина
    DefinePlugin для webpack

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,7 +3,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
     return [
         new HtmlWebpackPlugin({
@@ -14,5 +14,8 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: "css/[name].[contenthash:8].css"
         }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev)
+        })
     ]
 }
