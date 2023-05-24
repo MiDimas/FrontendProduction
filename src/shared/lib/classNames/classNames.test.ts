4de9bbc34f7e3cd919f357a15bd2e003646bebf8
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-05-24 17:10:58 +0800

    Добавлены тесты на функцию classNames
    Также добавлен пресет typescript на babel, для корректного запуска
    тестов на typescript в среде jest

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,27 @@
+import exp from 'constants';
+import { classNames } from './classNames';
+
 describe('classNames', () => {
-    test('test', () => {
-        expect(true).toBe(true);
+    test('with one param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2']))
+            .toBe(expected);
+    });
+    test('with mods class', () => {
+        const expected = 'someClass classGreen';
+        expect(classNames('someClass', { classRed: 1 > 10, classGreen: 10 > 1 }))
+            .toBe(expected);
+    });
+    test('with and additional class', () => {
+        const expected = 'someClass classRed classGreen class1 class2';
+        expect(classNames(
+            'someClass',
+            { classRed: true, classGreen: 10 > 1 },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
     });
 });
