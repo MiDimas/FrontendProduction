05f722e2bbd70ae799014307901469a0cfa01155
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-07-17 09:21:53 +0800

    Модалка переннесена в навбар
    Пока закомментирована, чтобы не устанавливать докер и не прогонять
    скриншотные тесты

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,25 +5,11 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import {
-    Suspense, useCallback, useEffect, useState,
+    Suspense, useCallback, useEffect,
 } from 'react';
-import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
-    const [isOpen, setIsOpen] = useState(false);
-
-    const onOpen = () => {
-        setIsOpen(true);
-    };
-    const onClose = () => {
-        setIsOpen(false);
-    };
-    const anyString = `
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur
-    consequuntur fuga placeat repudiandae sint voluptatum.
-    Beatae earum eligendi explicabo illo ipsum iusto laboriosam, quia quisquam
-    recusandae repellat tempore totam!`;
     const themeBody = useCallback(() => {
         document.body.className = classNames('app', {}, [theme]);
     }, [theme]);
@@ -33,11 +19,7 @@ function App() {
     return (
         <div>
             <Suspense fallback="">
-                <Modal isOpen={isOpen} onClose={onClose}>
-                    {anyString}
-                </Modal>
                 <Navbar />
-                <button onClick={onOpen} type="button">{'>>>'}</button>
                 <div className="content_page">
                     <Sidebar />
                     <AppRouter />
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,10 +1,37 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useCallback, useState } from 'react';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])} />
-);
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const closeHandler = useCallback(() => {
+        setIsOpen(false);
+    }, []);
+    const openHandler = useCallback(() => {
+        setIsOpen(true);
+    }, []);
+
+    const lorem = `Lorem ipsum dolor sit amet,
+     consectetur adipisicing elit. Nisi, voluptatum.`;
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            {/* <Button theme={ButtonTheme.BACKGROUND} className={cls.inks} onClick={openHandler}> */}
+            {/*    {t('Войти')} */}
+            {/* </Button> */}
+            {/* <Modal isOpen={isOpen} onClose={closeHandler}> */}
+            {/*    {lorem} */}
+            {/* </Modal> */}
+
+        </div>
+    );
+};
