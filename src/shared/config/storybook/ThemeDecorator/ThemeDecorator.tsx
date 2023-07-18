40350a1291c03049cc277096fb52187bc8d7caa7
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-07-18 21:32:04 +0800

    Обновлены тесты добавлены новые сторисы

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -2,8 +2,10 @@ import { StoryFn } from '@storybook/react';
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 
 export const ThemeDecorator = (theme: Theme) => (Story: StoryFn) => (
+    // <ThemeProvider initialTheme={theme}>
     <div className={`app ${theme}`}>
         <Story />
     </div>
+    // </ThemeProvider>
 
 );
