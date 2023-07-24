73e0a5221dbdd9a3d8f78a4c1ba97c97f60606c5
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-07-24 09:25:00 +0800

    Добавлены кастомные настройки сервера
    Также добавлен скрипт на запуск этого сервера

---
+++ json-server/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+const middlewares = jsonServer.defaults();
+
+const server = jsonServer.create();
+
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(async (req, res, next) => {
+    await  new Promise(res => setTimeout(res, 800));
+    next();
+});
+
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({message: "AUTH ERROR"});
+    }
+    next();
+})
+
+server.use(middlewares);
+server.use(router);
+
+
+server.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+    const {users} = db;
+
+    const userFromDb = users.find((user) => user.username === username && user.password === password);
+
+    if(userFromDb) {
+        return res.json(userFromDb)
+    }
+
+    return res.status(403).json({message: "AUTH ERROR"});
+
+});
+server.listen(8000, () => {
+    console.log('Сервер запущен на порту 8000')
+})
