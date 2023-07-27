3d47124e7dba10380cd08d0121ecee7600436a13
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-07-27 09:05:12 +0800

    Подключен редюсер для юзера
    Создан рутовый редюсер для всех редюсеров приложения

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,13 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore, ReducersMapObject} from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { StateSchema } from '../config/StateSchema';
+import {userReducer} from "entities/User";
 
-export const createReduxStore = (initialState?: StateSchema) => configureStore<StateSchema>(
-    {
-        reducer: {
-            counter: counterReducer,
+export const createReduxStore = (initialState?: StateSchema) => {
+    const rootReducer: ReducersMapObject<StateSchema> = {
+        counter: counterReducer,
+        user: userReducer
+    };
+    return configureStore<StateSchema>(
+        {
+            reducer: rootReducer,
+            devTools: __IS_DEV__,
+            preloadedState: initialState,
         },
-        devTools: __IS_DEV__,
-        preloadedState: initialState,
-    },
-);
+    );
+}
