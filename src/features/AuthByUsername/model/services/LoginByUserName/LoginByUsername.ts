8d6253c0a3004f34e08709e829e846a3b0014b78
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-08-15 09:07:36 +0800

    Перенесено сообщение об ошибки в компонент
    Чтобы не плодить доп декораторы для переводов
    И не ломать сторибук

--- src/features/AuthByUsername/model/services/LoginByUserName/LoginByUsername.ts
+++ src/features/AuthByUsername/model/services/LoginByUserName/LoginByUsername.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User, userActions } from 'entities/User';
 import axios from 'axios';
-import i18n from 'shared/config/i18n/i18n';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 interface LoginByUsernameProps {
@@ -21,7 +20,7 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {rej
             thunkAPI.dispatch(userActions.setAuthData(response.data));
             return response.data;
         } catch (e) {
-            return thunkAPI.rejectWithValue(i18n.t('Неправильный логин или пароль'));
+            return thunkAPI.rejectWithValue('error');
         }
     },
 );
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -37,7 +37,7 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
             <Text title={t('Форма авторизации')} />
-            {error && <Text text={error} theme={TextTheme.ERROR} />}
+            {error && <Text text={t('Неправильный логин или пароль')} theme={TextTheme.ERROR} />}
             <Input
                 type="text"
                 placeholder={t('Логин')}
