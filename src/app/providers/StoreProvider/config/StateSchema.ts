8e6599e4dc5087a4ca50ef348af4cfaa77e5b37f
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-08-17 09:37:29 +0800

    Добавлен редюсер менеджер
    Пока не до конца типизирован и не используется,
    но подключен к стору

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -5,5 +5,9 @@ import { LoginSchema } from 'features/AuthByUsername';
 export interface StateSchema {
     counter: CounterSchema;
     user: UserSchema;
-    loginForm: LoginSchema;
+
+    // Асинхронные редюсеры
+    loginForm?: LoginSchema;
 }
+
+export type StateSchemaKey = keyof StateSchema;
--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -3,18 +3,23 @@ import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { loginReducer } from 'features/AuthByUsername';
 import { StateSchema } from '../config/StateSchema';
+import { createReducerManager } from './reducerManager';
 
 export const createReduxStore = (initialState?: StateSchema) => {
     const rootReducer: ReducersMapObject<StateSchema> = {
         counter: counterReducer,
         user: userReducer,
-        loginForm: loginReducer,
+
     };
-    return configureStore<StateSchema>(
+    const reducerManager = createReducerManager(rootReducer);
+    const store = configureStore<StateSchema>(
         {
             reducer: rootReducer,
             devTools: __IS_DEV__,
             preloadedState: initialState,
         },
     );
+    // @ts-ignore
+    store.reducerManager = reducerManager;
+    return store;
 };
