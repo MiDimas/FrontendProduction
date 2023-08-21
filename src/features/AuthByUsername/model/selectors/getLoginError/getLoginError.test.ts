7fd227b9f97ea50088c5577a6ab022708f618a27
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-08-21 09:17:55 +0800

    Добавлен юнит тест на селектор ошибки в логине

---
+++ src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.test.ts
@@ -0,0 +1,14 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginError } from './getLoginError';
+
+describe('getLoginError тест', () => {
+    test('Тестирование возвращения ошибки', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                error: 'error',
+            },
+        };
+        expect(getLoginError(state as StateSchema)).toEqual('error');
+    });
+});
