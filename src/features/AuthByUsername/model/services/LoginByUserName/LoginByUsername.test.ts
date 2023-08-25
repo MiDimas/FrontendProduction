49b7dbb0dcdb38eb16d93dc1c668a98426018ac1
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-08-25 09:15:30 +0800

    Добавлен хэлпер для тестирования AsyncThunk
    Тесты LoginByUserName переписаны с использованием хэлпера

--- src/features/AuthByUsername/model/services/LoginByUserName/LoginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/LoginByUserName/LoginByUsername.test.ts
@@ -2,37 +2,63 @@ import axios from 'axios';
 import { Dispatch } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
 import { userActions } from 'entities/User';
+import { TestAsyncThunk } from 'shared/config/tests/TestAsyncThunk/TestAsyncThunk';
 import { loginByUsername } from './LoginByUsername';
 
 jest.mock('axios');
 const mockedAxios = jest.mocked(axios, true);
 
 describe('Тестирование AsyncThunk LoginByUsename', () => {
-    let dispatch: Dispatch;
-    let getState: () => StateSchema;
+    // let dispatch: Dispatch;
+    // let getState: () => StateSchema;
+    //
+    // beforeEach(() => {
+    //     dispatch = jest.fn();
+    //     getState = jest.fn();
+    // });
+    // ССтарый подход к тестированию
+    // test('Успешная авторизация', async () => {
+    //     const userValue = { username: '123', id: '1' };
+    //     mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+    //     const action = loginByUsername({ username: '123', password: '123' });
+    //     const result = await action(dispatch, getState, undefined);
+    //
+    //     expect(dispatch).toHaveBeenCalledTimes(3);
+    //     expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+    //     expect(mockedAxios.post).toHaveBeenCalled();
+    //     expect(result.meta.requestStatus).toBe('fulfilled');
+    //     expect(result.payload).toEqual(userValue);
+    // });
+    // test('Авторизация с ошибкой', async () => {
+    //     mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+    //     const action = loginByUsername({ username: '123', password: '123' });
+    //     const result = await action(dispatch, getState, undefined);
+    //
+    //     expect(dispatch).toHaveBeenCalledTimes(2);
+    //     expect(mockedAxios.post).toHaveBeenCalled();
+    //     expect(result.meta.requestStatus).toBe('rejected');
+    //     expect(result.payload).toEqual('error');
+    // });
+    // Тестирование с помощью объекта TestAsyncThunk
 
-    beforeEach(() => {
-        dispatch = jest.fn();
-        getState = jest.fn();
-    });
     test('Успешная авторизация', async () => {
         const userValue = { username: '123', id: '1' };
         mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
-        const action = loginByUsername({ username: '123', password: '123' });
-        const result = await action(dispatch, getState, undefined);
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: '123', password: '123' });
 
-        expect(dispatch).toHaveBeenCalledTimes(3);
-        expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+        expect(thunk.dispatch).toHaveBeenCalledTimes(3);
+        expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
         expect(mockedAxios.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('fulfilled');
         expect(result.payload).toEqual(userValue);
     });
     test('Авторизация с ошибкой', async () => {
         mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
-        const action = loginByUsername({ username: '123', password: '123' });
-        const result = await action(dispatch, getState, undefined);
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: '123', password: '123' });
 
-        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
         expect(mockedAxios.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('rejected');
         expect(result.payload).toEqual('error');
