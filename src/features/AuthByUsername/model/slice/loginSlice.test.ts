28726e157376cd410f503358be344ea34a3e066b
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-08-26 11:00:55 +0800

    Добавлены теты на логинСлайс

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,24 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/LoginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('Тестирование loginSlice', () => {
+    test('Проверка изменения поля username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'petux' };
+        expect(
+            loginReducer(
+                state as LoginSchema,
+                loginActions.setUsername('petushok'),
+            ),
+        ).toEqual({ username: 'petushok' });
+    });
+    test('Проверка изменения поля password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(
+            loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('cococo'),
+            ),
+        ).toEqual({ password: 'cococo' });
+    });
+});
