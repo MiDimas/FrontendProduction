50768da1cb236b3f1cf069d91ef779c01c8013ee
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-09-13 08:59:29 +0800

    Исправлены ошибки ts в конфиге сторибука

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,23 +10,24 @@ export default ({ config }: {config: webpack.Configuration}) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve.modules.push(paths.src);
-    config.resolve.extensions.push('.ts', '.tsx');
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    // @ts-ignore
+    config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
         return rule;
     });
 
-    config.module.rules.push({
+    config!.module!.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config.module.rules.push(buildCssLoaders(true));
-    config.plugins.push(new webpack.DefinePlugin({
+    config!.module!.rules.push(buildCssLoaders(true));
+    config!.plugins!.push(new webpack.DefinePlugin({
         __IS_DEV__: true,
         __API__: JSON.stringify(''),
     }));
