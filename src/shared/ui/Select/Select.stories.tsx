528d4e9524eed8736ab3aa87881ffb44c5a9a53e
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-09-24 14:58:43 +0800

    Добавлен ui компонент селект

---
+++ src/shared/ui/Select/Select.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Select } from './Select';
+
+const meta: Meta<typeof Select> = {
+    title: 'shared/Select',
+    component: Select,
+    argTypes: {
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof Select>;
+
+export const SelectLight: Story = {
+    args: {
+        label: 'выберите опцию',
+        options: [
+            { value: 'option1', content: 'Первая опция' },
+            { value: 'option2', content: 'Вторая опция' },
+        ],
+    },
+};
+export const SelectDark: Story = {
+    args: {
+        label: 'выберите опцию',
+        options: [
+            { value: 'option1', content: 'Первая опция' },
+            { value: 'option2', content: 'Вторая опция' },
+        ],
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
+export const SelectDisabled: Story = {
+    args: {
+        label: 'выберите опцию',
+        options: [
+            { value: 'option1', content: 'Первая опция' },
+            { value: 'option2', content: 'Вторая опция' },
+        ],
+        readonly: true,
+    },
+};
---
+++ src/shared/ui/Select/Select.tsx
@@ -0,0 +1,63 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import {
+    ChangeEvent, memo, useCallback, useMemo,
+} from 'react';
+import cls from './Select.module.scss';
+
+export interface SelectOption {
+    value: string;
+    content: string;
+}
+interface SelectProps {
+    className?: string;
+    label?: string;
+    options?: SelectOption[];
+    readonly?: boolean;
+    onChange?: (value: string) => void;
+    value?: string;
+}
+
+export const Select = memo((props: SelectProps) => {
+    const { t } = useTranslation();
+    const {
+        className,
+        label,
+        options,
+        readonly,
+        onChange,
+        value,
+    } = props;
+
+    const onChangeHandler = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+        onChange?.(e.target.value);
+    }, [onChange]);
+
+    const optionsList = useMemo(() => options?.map(({
+        value,
+        content,
+    }) => (
+        <option
+            value={value}
+            className={cls.option}
+            key={value}
+        >
+            {content}
+        </option>
+    )), [options]);
+
+    return (
+        <div className={classNames(cls.SelectWrapper, {}, [className])}>
+            {label
+                && <span className={cls.label}>{label}</span>}
+            <select
+                className={cls.select}
+                value={value}
+                onChange={onChangeHandler}
+                disabled={readonly}
+            >
+                {optionsList}
+            </select>
+        </div>
+    );
+});
