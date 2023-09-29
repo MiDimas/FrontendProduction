17371322d6078b042f00c79b4f6941fffb78509d
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-09-29 09:24:25 +0800

    Добавлена валидация полей профайла
    Так же добавлены коды ошибок профиля

--- src/features/EditableProfileCard/model/services/UpdateProfileData/UpdateProfileData.ts
+++ src/features/EditableProfileCard/model/services/UpdateProfileData/UpdateProfileData.ts
@@ -1,19 +1,29 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Profile } from 'entities/Profile';
-import { StateSchema, ThunkConfig } from 'app/providers/StoreProvider';
+import { ThunkConfig } from 'app/providers/StoreProvider';
+import { ValidateProfileError } from '../../types/ProfileSchema';
 import { getProfileForm } from '../../selectors/getProfileForm/getProfileForm';
+import { validateProfileData } from '../ValidateProfileData/ValidateProfileData';
 
-export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
+export const updateProfileData = createAsyncThunk<
+    Profile,
+    void,
+    ThunkConfig<ValidateProfileError[]>
+>(
     'profile/updateProfileData',
     async (_, thunkAPI) => {
         const { extra, rejectWithValue, getState } = thunkAPI;
         const { api } = extra;
         const formData = getProfileForm(getState());
+        const errors = validateProfileData(formData);
+        if (errors.length) {
+            return rejectWithValue(errors);
+        }
         try {
             const response = await api.put<Profile>('/profile', formData);
             return response.data;
         } catch (e) {
-            return rejectWithValue('error');
+            return rejectWithValue([ValidateProfileError.SERVER_ERROR]);
         }
     },
 );
--- src/features/EditableProfileCard/model/types/ProfileSchema.ts
+++ src/features/EditableProfileCard/model/types/ProfileSchema.ts
@@ -1,9 +1,16 @@
 import { Profile } from 'entities/Profile';
 
+export enum ValidateProfileError {
+    INCORRECT_USER_DATA = 'INCORRECT_USER_DATA',
+    INCORRECT_AGE = 'INCORRECT_AGE',
+    INCORRECT_COUNTRY = 'INCORRECT_COUNTRY',
+    NO_DATA = 'NO_DATA',
+    SERVER_ERROR = 'SERVER_ERROR',
+}
 export interface ProfileSchema {
     data?: Profile;
     form?: Profile;
     isLoading: boolean;
-    error?: string;
+    error?: ValidateProfileError[];
     readonly: boolean;
 }
