2768d6c732b3d9700f0a3a6db535553acfa4d306
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-10-06 09:16:01 +0800

    Добавлен тест на валидации

---
+++ src/features/EditableProfileCard/model/services/ValidateProfileData/ValidateProfileData.test.ts
@@ -0,0 +1,71 @@
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { ValidateProfileError } from 'features/EditableProfileCard/model/types/ProfileSchema';
+import { validateProfileData } from './ValidateProfileData';
+
+const data = {
+    firstname: 'Peter',
+    lastname: 'Parker',
+    age: 25,
+    username: 'spider',
+    city: 'New-York',
+    country: Country.Kazakhstan,
+    currency: Currency.USD,
+};
+describe('ValidateProfileData', () => {
+    test('Полностью корректно заполнено', () => {
+        const result = validateProfileData(
+            {
+                ...data,
+            },
+        );
+        expect(result).toEqual([]);
+    });
+    test('Проверка валидации на незаполненое поле имени и фамилии', () => {
+        const result = validateProfileData(
+            {
+                ...data,
+                firstname: '',
+            },
+        );
+        expect(result).toEqual([ValidateProfileError.INCORRECT_USER_DATA]);
+    });
+    test('Проверка валидации на незаполненое поле возраста', () => {
+        const result = validateProfileData(
+            {
+                ...data,
+                age: 0,
+            },
+        );
+        expect(result).toEqual([ValidateProfileError.INCORRECT_AGE]);
+    });
+    test('Проверка валидации на незаполненое поле страны', () => {
+        const result = validateProfileData(
+            {
+                ...data,
+                country: undefined,
+            },
+        );
+        expect(result).toEqual([ValidateProfileError.INCORRECT_COUNTRY]);
+    });
+    test('Проверка валидации на незаполненые обязательные поля', () => {
+        const result = validateProfileData(
+            {
+                ...data,
+                firstname: '',
+                lastname: '',
+                age: 0,
+                country: undefined,
+            },
+        );
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_USER_DATA,
+            ValidateProfileError.INCORRECT_AGE,
+            ValidateProfileError.INCORRECT_COUNTRY,
+        ]);
+    });
+    test('Нет профиля', () => {
+        const result = validateProfileData();
+        expect(result).toEqual([ValidateProfileError.NO_DATA]);
+    });
+});
