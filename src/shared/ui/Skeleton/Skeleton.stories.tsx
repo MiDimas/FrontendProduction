986efdbb1ad00f70317608e0b523d36adfd5de51
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-10-22 14:00:29 +0800

    Добавлен компонент скелетона
    так же подготовлены сторибук кейсы, для скелетонов

---
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Skeleton } from './Skeleton';
+
+const meta: Meta<typeof Skeleton > = {
+    title: 'shared/Skeleton',
+    component: Skeleton,
+    argTypes: {
+
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof Skeleton>;
+
+export const SkeletonNormal = {
+    args: {
+        height: 200,
+        width: '100%',
+    },
+};
+
+export const SkeletonCircle = {
+    args: {
+        height: 100,
+        width: 100,
+        border: '50%',
+    },
+};
+
+export const SkeletonNormalDark = {
+    args: {
+        height: 200,
+        width: '100%',
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
+
+export const SkeletonCircleDark = {
+    args: {
+        height: 100,
+        width: 100,
+        border: '50%',
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
---
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -0,0 +1,34 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { CSSProperties, memo } from 'react';
+import cls from './Skeleton.module.scss';
+
+interface SkeletonProps {
+    className?: string;
+    height?: string | number;
+    width?: string | number;
+    border?: string;
+}
+
+export const Skeleton = memo((props: SkeletonProps) => {
+    const {
+        className,
+        height,
+        width,
+        border,
+    } = props;
+
+    const styles: CSSProperties = {
+        borderRadius: border,
+        width,
+        height,
+    };
+
+    return (
+        <div
+            className={
+                classNames(cls.Skeleton, {}, [className])
+            }
+            style={styles}
+        />
+    );
+});
