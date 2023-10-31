5b3426684d702a6c685cec8881c680d87434fb90
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-10-31 09:38:35 +0800

    Добавлен сторикейс на ArticleDetails

---
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
@@ -0,0 +1,119 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Article, ArticleBlockType, ArticleType } from 'entities/Article/model/types/article';
+import { ArticleDetails } from './ArticleDetails';
+
+const meta: Meta<typeof ArticleDetails> = {
+    title: 'entities/ArticleDetails',
+    component: ArticleDetails,
+    argTypes: {
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ArticleDetails>
+
+const article: Article = {
+    id: '1',
+    title: 'JavaScript News',
+    subtitle: 'Что нового в JS в этом году',
+    img: 'https://teknotower.com/wp-content/uploads/2020/11/js.png',
+    views: 1022,
+    createdAt: '26.10.2022',
+    type: [
+        ArticleType.IT,
+    ],
+    blocks: [
+        {
+            id: '1',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                // eslint-disable-next-line
+                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
+                // eslint-disable-next-line
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
+                // eslint-disable-next-line
+                'Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:',
+            ],
+        },
+        {
+            id: '4',
+            type: ArticleBlockType.CODE,
+            // eslint-disable-next-line
+            code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
+        },
+        {
+            id: '5',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                // eslint-disable-next-line
+                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
+                // eslint-disable-next-line
+                'Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:',
+            ],
+        },
+        {
+            id: '2',
+            type: ArticleBlockType.IMAGE,
+            // eslint-disable-next-line
+            src: 'https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png',
+            title: 'Рисунок 1 - скриншот сайта',
+        },
+        {
+            id: '3',
+            type: ArticleBlockType.CODE,
+            // eslint-disable-next-line
+            code: "const path = require('path');\n\nconst server = jsonServer.create();\n\nconst router = jsonServer.router(path.resolve(__dirname, 'db.json'));\n\nserver.use(jsonServer.defaults({}));\nserver.use(jsonServer.bodyParser);",
+        },
+        {
+            id: '7',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                // eslint-disable-next-line
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
+                // eslint-disable-next-line
+                'Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:',
+            ],
+        },
+        {
+            id: '8',
+            type: ArticleBlockType.IMAGE,
+            // eslint-disable-next-line
+            src: 'https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png',
+            title: 'Рисунок 1 - скриншот сайта',
+        },
+        {
+            id: '9',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                // eslint-disable-next-line
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
+            ],
+        },
+    ],
+};
+export const ArticleDetailsNormal: Story = {
+    args: {
+        id: '1',
+    },
+    decorators: [StoreDecorator({ articleDetails: { data: article } })],
+};
+
+export const ArticleDetailsLoading: Story = {
+    args: {
+        id: '1',
+    },
+    decorators: [StoreDecorator({ articleDetails: { isLoading: true } })],
+};
+
+export const ArticleDetailsError: Story = {
+    args: {
+        id: '1',
+    },
+    decorators: [StoreDecorator({ articleDetails: { isLoading: false, error: 'Ошибка' } })],
+};
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -4,10 +4,12 @@ import { ReducersMapObject } from '@reduxjs/toolkit';
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 import { ReducersList } from 'shared/lib/DynamicModuleLoader/DynamicModuleLoader';
 import { profileReducer } from 'features/EditableProfileCard';
+import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
 
 const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
     profile: profileReducer,
+    articleDetails: articleDetailsReducer,
 };
 export const StoreDecorator = (
     state: DeepPartial<StateSchema>,
