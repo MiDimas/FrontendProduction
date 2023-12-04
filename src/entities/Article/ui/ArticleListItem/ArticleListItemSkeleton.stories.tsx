ed0d0b8054e805e1a6611c0de79767099a911274
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-12-04 10:09:27 +0800

    Добавлен сторикейс для скелетонов ArticleListItem

---
+++ src/entities/Article/ui/ArticleListItem/ArticleListItemSkeleton.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ArticleView } from 'entities/Article';
+import { ArticleListItemSkeleton } from './ArticleListItemSkeleton';
+
+const meta: Meta<typeof ArticleListItemSkeleton> = {
+    title: 'entities/Article/ArticleListItemSkeleton',
+    component: ArticleListItemSkeleton,
+    argTypes: {
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ArticleListItemSkeleton>
+
+export const ArticleListItemSkeletonLightSmall: Story = {
+    args: {
+        view: ArticleView.SMALL,
+    },
+};
+export const ArticleListItemSkeletonDarkSmall: Story = {
+    args: {
+        view: ArticleView.SMALL,
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
+
+export const ArticleListItemSkeletonLightBig: Story = {
+    args: {
+        view: ArticleView.BIG,
+    },
+};
+export const ArticleListItemSkeletonDarkBig: Story = {
+    args: {
+        view: ArticleView.BIG,
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
