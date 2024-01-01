b7996611e656a07902e39d268fc26650aa9e7406
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-01-01 16:38:04 +0800

    Добавлена стори для вью селектора

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ArticleViewSelector } from './ArticleViewSelector';
+import { ArticleView } from '../../model/types/article';
+
+const meta: Meta<typeof ArticleViewSelector> = {
+    title: 'ArticleViewSelector',
+    component: ArticleViewSelector,
+    argTypes: {
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ArticleViewSelector>
+
+export const ArticleViewSelectorNormal: Story = {
+    args: {
+        view: ArticleView.SMALL,
+    },
+};
+export const ArticleViewSelectorDark: Story = {
+    args: {
+        view: ArticleView.SMALL,
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
