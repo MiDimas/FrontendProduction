7ee17efdb4e29e6e585b7570782acbeb3c3457b1
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-03-10 10:20:25 +0800

    Добавлен шаблон для редаакс слайса

--- scripts/createSlice/templates/createModel.js
+++ scripts/createSlice/templates/createModel.js
@@ -1,5 +1,7 @@
 const fs = require('fs/promises');
 
+const reduxSliceTemplate = require('./reduxSliceTemplate');
+
 const resolveRoot = require('../resolveRoot');
 
 module.exports = async (layer, sliceName) => {
@@ -14,6 +16,17 @@ module.exports = async (layer, sliceName) => {
             fs.mkdir(resolveModulePath('services'));
         } catch (e) {
             console.log(`Создание model для слайса ${sliceName} не удалось`);
+        }
+    };
+
+    const createReduxSlice = async () => {
+        try {
+            fs.writeFile(
+                resolveModulePath('slices', `${sliceName}Slice.ts`),
+                reduxSliceTemplate(sliceName),
+            );
+        } catch (e) {
+            console.log('Ошибка при создании редакс-слайса', e);
         }
     };
 };
