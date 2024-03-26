64aacefd6a4903dcc712022d6d927809d5a48da5
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-03-26 09:12:29 +0800

    Добавлено прокидывание асинк редюсеров в рендерТест

--- src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
+++ src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
@@ -1,12 +1,36 @@
 import { screen } from '@testing-library/react';
-import {
-    renderComponent,
-} from 'shared/config/tests/renderWithTranslation/renderComponent';
+import { renderComponent } from 'shared/config/tests/renderWithTranslation/renderComponent';
+import { Profile } from 'entities/Profile';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+import { profileReducer } from '../../model/slice/profileSlice';
 import { EditableProfileCard } from './EditableProfileCard';
 
+const profile: Profile = {
+    id: '1',
+    firstname: 'admin',
+    lastname: 'admin2',
+    age: 120,
+    currency: Currency.RUB,
+    country: Country.Armenia,
+    city: 'Erevan',
+    username: 'adman',
+};
 describe('Проверка компонента EditableProfileCard', () => {
     test('Тестирование отрисовки компонента', () => {
-        renderComponent(<EditableProfileCard />);
+        renderComponent(<EditableProfileCard id="1" />, {
+            initialState: {
+                profile: {
+                    readonly: true,
+                    data: profile,
+                    form: profile,
+                },
+            },
+            asyncReducers: {
+                profile: profileReducer,
+
+            },
+        });
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 });
--- src/shared/config/tests/renderWithTranslation/renderComponent.tsx
+++ src/shared/config/tests/renderWithTranslation/renderComponent.tsx
@@ -4,23 +4,27 @@ import { ReactNode } from 'react';
 import i18nForTest from 'shared/config/i18n/i18nForTest';
 import { MemoryRouter } from 'react-router-dom';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { Action, ReducersMapObject } from '@reduxjs/toolkit';
+import { ReducersMapObject } from '@reduxjs/toolkit';
+import { ReducersList } from 'shared/lib/DynamicModuleLoader/DynamicModuleLoader';
 
 export interface RenderComponentOption {
     route?: string;
     initialState?: DeepPartial<StateSchema>;
-    asyncReducers?: ReducersMapObject<StateSchema, Action<any>>
+    asyncReducers?: ReducersList;
 }
-
 export function renderComponent(component: ReactNode, option: RenderComponentOption = {}) {
     const {
         route = '/',
         initialState,
         asyncReducers,
     } = option;
+
     return render(
         <MemoryRouter initialEntries={[route]}>
-            <StoreProvider asyncReducers={asyncReducers} initialState={initialState as StateSchema}>
+            <StoreProvider
+                asyncReducers={asyncReducers as ReducersMapObject<StateSchema>}
+                initialState={initialState as StateSchema}
+            >
                 <I18nextProvider i18n={i18nForTest}>
                     {component}
                 </I18nextProvider>
