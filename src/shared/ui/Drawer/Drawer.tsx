7d8ddabb217d4aa2dc3e2947f8b4e1e33e61213e
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-04-20 22:56:23 +0800

    Дроуер переносится на drag&drop

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,8 +1,11 @@
-import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import { lazy, memo, ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import {
+    memo, ReactNode, useCallback, useEffect,
+} from 'react';
 import { Overlay } from 'shared/ui/Overlay/Overlay';
 import { Portal } from 'shared/ui/Portal/Portal';
-import { useModal } from 'shared/lib/hooks/useModal/useModal';
+import { a, config, useSpring } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
 import cls from './Drawer.module.scss';
 
 interface DrawerProps {
@@ -13,7 +16,9 @@ interface DrawerProps {
     lazy?: boolean;
 }
 
+const height = window.innerHeight - 100;
 export const Drawer = memo((props: DrawerProps) => {
+    const [{ y }, api] = useSpring(() => ({ y: height }));
     const {
         className,
         children,
@@ -21,32 +26,75 @@ export const Drawer = memo((props: DrawerProps) => {
         onClose,
         lazy,
     } = props;
-    const {
-        isClosing,
-        isOpening,
-        isMounted,
-        close,
-    } = useModal({
-        onClose,
-        isOpen,
+    // const {
+    //     isClosing,
+    //     isOpening,
+    //     isMounted,
+    //     close,
+    // } = useModal({
+    //     onClose,
+    //     isOpen,
+    // });
+    // const mods:Mods = {
+    //     [cls.opened]: isOpen && !isOpening,
+    //     [cls.closing]: isClosing,
+    // };
+
+    const openDrawer = useCallback(() => {
+        api.start({ y: 0, immediate: false });
+    }, [api]);
+
+    useEffect(() => {
+        if (isOpen) {
+            openDrawer();
+        }
+    }, [isOpen, openDrawer, api]);
+
+    const close = useCallback((velocity = 0) => {
+        api.start({
+            y: height, immediate: false, config: { ...config.stiff, velocity }, onResolve: onClose,
+        });
+    }, [api, onClose]);
+
+    const bind = useDrag(({
+        last,
+        velocity: [, vy],
+        direction: [, dy],
+        movement: [, my],
+        cancel,
+    }) => {
+        if (my < -70) cancel();
+        if (last) {
+            if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+                close();
+            } else {
+                openDrawer();
+            }
+        } else {
+            api.start({ y: my, immediate: false });
+        }
+    }, {
+        from: [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true,
     });
-    const mods:Mods = {
-        [cls.opened]: isOpen && !isOpening,
-        [cls.closing]: isClosing,
-    };
-    if (lazy && !isMounted) {
+
+    if (!isOpen) {
         return null;
     }
+    const display = y.to((py) => (py < height ? 'block' : 'none'));
     return (
         <Portal>
             <div className={
-                classNames(cls.Drawer, mods, [className])
+                classNames(cls.Drawer, {}, [className])
             }
             >
                 <Overlay onClick={close} />
-                <div className={cls.content}>
+                <a.div
+                    className={cls.sheet}
+                    style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
+                    {...bind()}
+                >
                     {children}
-                </div>
+                </a.div>
             </div>
         </Portal>
 
