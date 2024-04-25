40076fdef1875bbc165065cbd9e00af14f5dda3f
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-04-25 16:04:39 +0800

    Добавлен сборщик vite
    Теперь для дев сборки можно использовать vite
    который работает быстрее чем вебпак

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [
+        svgr({ include: '**/*.svg' }),
+        react(),
+    ],
+    resolve: { alias: [{ find: '@', replacement: '/src' }] },
+    define: {
+        __API__: JSON.stringify('http://localhost:8000'),
+        __IS_DEV__: JSON.stringify(true),
+        __PROJECT__: JSON.stringify('frontend'),
+    },
+});
