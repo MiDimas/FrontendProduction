e7ac82c6dd0f4c382a05e50c3813c46b39feea2d
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-06-13 08:02:03 +0800

    Added the AppImage component
    The component is neefed for error and loading handling

---
+++ src/shared/ui/AppImage/AppImage.tsx
@@ -0,0 +1,42 @@
+import {
+    ImgHTMLAttributes, ReactElement, useLayoutEffect, useState,
+} from 'react';
+
+interface AppImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+    fallback?: ReactElement;
+    errorFallback?: ReactElement;
+}
+export const AppImage = (props: AppImageProps) => {
+    const {
+        className,
+        fallback,
+        errorFallback,
+        src,
+        alt = 'image',
+        ...otherProps
+    } = props;
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
+
+    useLayoutEffect(() => {
+        const img = new Image();
+        img.src = src ?? '';
+        img.onload = () => {
+            setIsLoading(false);
+        };
+        img.onerror = () => {
+            setIsLoading(false);
+            setHasError(true);
+        };
+    }, [src]);
+
+    if (isLoading && fallback) {
+        return fallback;
+    }
+
+    if (hasError && errorFallback) {
+        return errorFallback;
+    }
+
+    return <img src={src} alt={alt} className={className} {...otherProps} />;
+};
