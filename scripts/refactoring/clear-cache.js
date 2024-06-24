394a41a61ff8d80a3624b8ad0f5faa656bf01a6a
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-06-24 08:38:28 +0800

    updated loki to version 0.35.0
    fix bug with clearCache if .cache wasn't found

--- scripts/refactoring/clear-cache.js
+++ scripts/refactoring/clear-cache.js
@@ -4,10 +4,14 @@ const path = require('path');
 
 try {
     (async () => {
-        const cacheDir = path.resolve(__dirname, '..', '..', 'node_modules', '.cache');
-        await fsp.access(cacheDir);
-        await fsp.rm(cacheDir, { recursive: true });
-        console.log('cache was delete');
+        try {
+            const cacheDir = path.resolve(__dirname, '..', '..', 'node_modules', '.cache');
+            await fsp.access(cacheDir);
+            await fsp.rm(cacheDir, { recursive: true });
+            console.log('cache was delete');
+        } catch (e) {
+            console.log("cache wasn't found");
+        }
     })();
 } catch {
     console.log("cache didn't clear");
