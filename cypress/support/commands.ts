7fdd725be93adbd20b319118d1dd3d2a11363f1d
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-07-07 15:59:01 +0800

    added comment commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,7 +1,9 @@
 import * as commonCommands from './commands/common';
 import * as profileCommands from './commands/profile';
+import * as commentCommands from './commands/comments';
 
 Cypress.Commands.addAll(commonCommands);
 Cypress.Commands.addAll(profileCommands);
+Cypress.Commands.addAll(commentCommands);
 
 export {};
