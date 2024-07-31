d6c1a8da09e7854db4fbb6c3cd2d8bad42bdce00
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-07-31 19:26:01 +0800

    added story shared Code

---
+++ src/shared/ui/Code/Code.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Code } from './Code';
+import {ThemeDecorator} from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "@/shared/const/theme";
+
+
+const meta: Meta<typeof Code> = {
+    title: 'Code',
+    component: Code,
+    argTypes: {
+    },
+    args: {
+        text: 'print("hello world")'
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Code>
+
+export const CodeNormal: Story = {
+    args: {},
+};
+export const CodeDark: Story = {
+    args: {},
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
