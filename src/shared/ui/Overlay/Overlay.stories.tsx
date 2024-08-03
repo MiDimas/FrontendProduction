90134cd667bb767a8a68ed09ddd6cb079ce73758
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-08-03 14:30:11 +0800

    added overlay story

---
+++ src/shared/ui/Overlay/Overlay.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react';
+import {ThemeDecorator} from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { Overlay } from './Overlay';
+import {Theme} from "@/shared/const/theme";
+
+const meta: Meta<typeof Overlay> = {
+    title: 'Overlay',
+    component: Overlay,
+    argTypes: {
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Overlay>
+
+export const OverlayNormal: Story = {
+    args: {},
+};
+export const OverlayDark: Story = {
+    args: {},
+    decorators: [ ThemeDecorator(Theme.DARK)],
+};
