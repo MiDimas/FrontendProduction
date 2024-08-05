10646c3e1e45c56b0d3db371ff01b46215734e1c
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-08-05 11:15:17 +0800

    fix storybook components and reshoted ui tests

--- src/pages/ForbiddenPage/ui/ForbiddenPage.stories.tsx
+++ src/pages/ForbiddenPage/ui/ForbiddenPage.stories.tsx
@@ -2,12 +2,14 @@ import { Meta, StoryObj } from '@storybook/react';
 import { ForbiddenPage } from './ForbiddenPage';
 import {ThemeDecorator} from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import {Theme} from "@/shared/const/theme";
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 const meta: Meta<typeof ForbiddenPage> = {
     title: 'pages/ForbiddenPage',
     component: ForbiddenPage,
     argTypes: {
     },
+    decorators: [StoreDecorator({})],
 };
 
 export default meta;
--- src/shared/ui/AppImage/AppImage.stories.tsx
+++ src/shared/ui/AppImage/AppImage.stories.tsx
@@ -8,14 +8,14 @@ import {Skeleton} from "@/shared/ui/Skeleton";
 
 
 const meta: Meta<typeof AppImage> = {
-    title: 'AppImage',
+    title: 'shared/AppImage',
     component: AppImage,
     argTypes: {
     },
     args: {
         fallback: <Skeleton width="100%" height="200px" />,
         alt: "image",
-        src: "/shared/assets/tests/avatar.webp"
+        src: "https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png"
     }
 };
 
