40148c5599ee5008c93c1f5cdd5d00f61a5f4689
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-08-08 08:31:02 +0800

    added cases in VStack story

--- src/shared/ui/Stack/VStack/VStack.stories.tsx
+++ src/shared/ui/Stack/VStack/VStack.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import { VStack } from './VStack';
 
 const meta: Meta<typeof VStack> = {
-    title: 'VStack',
+    title: 'shared/VStack',
     component: VStack,
     argTypes: {
     },
@@ -28,3 +28,22 @@ export const VStackNormal: Story = {
 export const VStackJustifyEnd: Story = {
     args: {justify: "end"},
 };
+
+export const VStackJustifyBetween: Story = {
+    args: {justify: "between"},
+};
+export const VStackJustifyCenter: Story = {
+    args: {justify: "center"},
+};
+export const VStackAlignStart: Story =  {
+    args: {align: 'start'}
+}
+export const VStackAlignCenter: Story =  {
+    args: {align: 'center'}
+}
+export const VStackAlignEnd: Story =  {
+    args: {align: 'end'}
+}
+export const VStackAlignBaseline: Story =  {
+    args: {align: 'baseline'}
+}
