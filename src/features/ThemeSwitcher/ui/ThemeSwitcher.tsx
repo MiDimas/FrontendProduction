7bc5ff6facd95be8a3b75421479e61ec3d72b234
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-11-01 07:52:02 +0800

    changed the toggle theme function
    now this have callback param which makes saving that you want

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,10 +1,11 @@
-import { memo } from 'react';
+import {memo, useCallback} from 'react';
 import ThemeDark from '@/shared/assets/icons/themeDark.svg';
 import ThemeLight from '@/shared/assets/icons/themeLight.svg';
 import { Theme } from '@/shared/const/theme';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { Button, ButtonTheme } from '@/shared/ui/Button';
+import {LOCAL_STORAGE_THEME_KEY} from "@/shared/const/localstorage";
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -13,11 +14,16 @@ interface ThemeSwitcherProps {
 
 export const ThemeSwitcher = memo(({ className, startTheme }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
+    const toggleThemeHandler = useCallback(() => {
+        toggleTheme?.((theme) =>
+            localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme || '')
+        )
+    }, [toggleTheme]);
     return (
         <Button
             className={classNames('', {}, [className])}
             theme={ButtonTheme.CLEAR}
-            onClick={toggleTheme}
+            onClick={toggleThemeHandler}
         >
             {theme === Theme.DARK || startTheme === Theme.DARK ? <ThemeDark /> : <ThemeLight />}
         </Button>
--- src/shared/lib/hooks/useTheme/useTheme.ts
+++ src/shared/lib/hooks/useTheme/useTheme.ts
@@ -1,17 +1,16 @@
 import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME_KEY } from '@/shared/const/localstorage';
 import { Theme } from '@/shared/const/theme';
 import { ThemeContext } from '../../context/ThemeContext';
 
 interface UseThemeResult {
-    toggleTheme: () => void;
+    toggleTheme: (saveAction?: (theme?: Theme) => void) => void;
     theme: Theme;
 }
 
 export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
-    const toggleTheme = () => {
+    const toggleTheme = (saveAction?: (theme?:Theme) => void) => {
         let newTheme: Theme;
         switch (theme) {
             case Theme.LIGHT:
@@ -27,7 +26,8 @@ export function useTheme(): UseThemeResult {
                 newTheme = Theme.LIGHT;
         }
         setTheme?.(newTheme);
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+        // localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+        saveAction?.(newTheme);
     };
 
     return {
