69805b52794ab0096a40f74c821e6be9ef7cb7d0
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-11-02 07:37:54 +0800

    added userapi for query setJsonSettings

---
+++ src/entities/User/api/userApi.ts
@@ -0,0 +1,24 @@
+import { rtkApi } from "@/shared/api/rtkApi";
+import {User} from "..";
+import {JsonSettings} from '../model/types/jsonSettings';
+
+interface SetJsonSettingsArg {
+    userId: string;
+    jsonSettings: JsonSettings;
+}
+
+const userApi = rtkApi.injectEndpoints({
+    endpoints: (build) => ({
+        setJsonSettings: build.mutation<User, SetJsonSettingsArg>({
+            query: ({userId, jsonSettings}) => ({
+                url: `/users/${userId}`,
+                method: 'PATCH',
+                body: {
+                    jsonSettings,
+                }
+            })
+        })
+    })
+})
+
+export const setJsonSettingsMutation = userApi.endpoints.setJsonSettings.initiate;
