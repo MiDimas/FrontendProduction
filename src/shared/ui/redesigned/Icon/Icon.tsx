33353593f073fd380c72473bc1bffe160b11661c
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-12-15 18:25:41 +0800

    prepare clicable icon

--- src/shared/ui/redesigned/Icon/Icon.tsx
+++ src/shared/ui/redesigned/Icon/Icon.tsx
@@ -2,19 +2,51 @@ import React from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Icon.module.scss';
 
-interface IconProps extends React.SVGProps<SVGSVGElement> {
+type SvgProps = Omit<React.SVGProps<SVGSVGElement>, "onClick">
+interface IconBaseProps extends SvgProps {
     className?: string;
     Svg: React.VFC<React.SVGProps<SVGSVGElement>>;
 }
-
+interface ClickableIcon extends IconBaseProps{
+    clickable: true;
+    onClick: () => void;
+}
+interface NonClickableIcon extends IconBaseProps {
+    clickable: false;
+}
+type IconProps = ClickableIcon | NonClickableIcon;
 export const Icon = (props: IconProps) => {
     const {
         className,
         Svg,
         width = 32,
         height = 32,
+        clickable,
         ...otherProps
     } = props;
+
+    const icon = (
+        <Svg
+            className={classNames(cls.Icon, {}, [
+                className,
+            ])}
+            width={width}
+            height={height}
+            {...otherProps}
+        />
+    );
+
+    if(clickable){
+        const {onClick} = props
+        return (
+            <button
+            className={cls.button}
+            type='button'
+            onClick={onClick}
+        >
+            {icon}
+        </button>)
+    }
     return (
         <Svg
             className={classNames(cls.Icon, {}, [
